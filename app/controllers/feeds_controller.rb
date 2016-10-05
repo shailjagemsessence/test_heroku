daea8eb9513322216e35ac7f04d7e18849d1f06e
shailjagemsessence <shailja@gemsessence.com>
2016-10-05 15:37:02 +0530

    feed

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -1,9 +1,9 @@
 class FeedsController < ApplicationController
   before_filter :authenticate_user!
-
   def index
-    @feeds = current_user.feeds
+    @feeds = Feed.by_permission.order(:updated_at)
   end 
+    
   def new
     # binding.pry
     @feed = Feed.new
@@ -12,27 +12,29 @@ class FeedsController < ApplicationController
   def show
        # binding.pry
     @feed = current_user.feeds.find(params[:id])
-
   end
-
 
   def create
     # binding.pry
     @feed = current_user.feeds.build(feed_params)
     if @feed.save!
       redirect_to feeds_path
+      flash[:notice] = "Successfully submitted details!"
     else
       render :new
     end
   end
 
   def bookmarks
-    @feeds = current_user.feeds.all
+    @feeds = current_user.feeds.bookmark_records
   end
   
 private
   def feed_params
     params.require(:feed).permit(:body, :image, :permission, :user_id, :bookmark)
   end
+
 end
+
+
      
--- app/models/feed.rb
+++ app/models/feed.rb
@@ -1,9 +1,11 @@
 class Feed < ApplicationRecord
   belongs_to :user
-  enum permission: [ :view_all, :me, :friend]
+  enum permission: [:view_all, :me, :friend]
   mount_uploader :image, AvatarUploader
-  
+  scope :bookmark_records, -> {where(bookmark: true)}
+  scope :by_permission, -> {where(permission: 0)}
 end
+  
 
 
  
