75aebe75d981af320ee3fd4e0f5144cbdb08e68c
shailjagemsessence <shailja@gemsessence.com>
2016-10-17 19:43:14 +0530

    login css

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,12 +7,12 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-     # binding.pry
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :contact_no, :dob, :email, :password])
   end
-
+    
   before_filter :authenticate_user!
 end
+
 
 
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  has_many :feeds
+  has_many :feeds, :dependent => :destroy
+ 
   has_many :friendships
   # has_many :friends, class_name: "User", foreign_key: 'friend_id'
   # belongs_to :user, class_name: "User"
