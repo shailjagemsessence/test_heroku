ae68bba0b3af487cd8e2e47db5775fb636a215bd
shailjagemsessence <shailja@gemsessence.com>
2016-10-18 18:52:29 +0530

    send mail done

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable, :confirmable,:registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :feeds, :dependent => :destroy
  
@@ -17,7 +17,7 @@ end
   
   
   
-  
+
 
   
   
